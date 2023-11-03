0a2437ad824304782502457981a36331e7375e62
Nova Fallen <nfallen@google.com>
2023-11-03 17:55:01 +0000

    Add license headers to scripts.
    Also change the copyright in all licenses to Google LLC.
    
    Change-Id: I5ec927007bd607c0611030492e17ad1e940b1513

--- scripts/export_container_bundle.sh
+++ scripts/export_container_bundle.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-
+#
+# Copyright 2023 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 # Script to export a container image and convert it into an OCI Runtime Bundle.
 # (https://github.com/opencontainers/runtime-spec/blob/4e3b9264a330d094b0386c3703c5f379119711e8/bundle.md)
 #
