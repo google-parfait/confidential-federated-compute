ed69b9d1a57efcca96f5c26fee5a7f0634095b89
Zoe Gong <zgong@google.com>
2024-01-04 02:02:56 +0000

    Add licenses to files missing them.
    Change-Id: Ic5248be08622d458db08370f3300e1ed7f07f11d

--- containers/sql_server/sqlite_adapter.h
+++ containers/sql_server/sqlite_adapter.h
@@ -1,3 +1,19 @@
+// Copyright 2024 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#ifndef CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_SQL_SERVER_SQLITE_ADAPTER_H_
+#define CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_SQL_SERVER_SQLITE_ADAPTER_H_
+
 #include <memory>
 
 #include "absl/base/attributes.h"
@@ -65,3 +81,5 @@ class SqliteAdapter {
 };
 
 }  // namespace confidential_federated_compute::sql_server
+
+#endif  // CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_SQL_SERVER_SQLITE_ADAPTER_H_
