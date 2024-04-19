f1575c77baf1c76e0930bc9c16f0b05f1a9b6ed7
Brett McLarnon <bmclarnon@google.com>
2024-04-18 17:41:37 -0700

    Make generate_test_computations.py standalone.
    This is the only Python-based tool in the repository, and since it isn't
    run/tested as part of continuous integration, building it with bazel
    doesn't increase test coverage. Instead, we can provide instructions for
    running it via a python venv, which allows the build setup to be
    simplified significantly.
    
    Change-Id: I403deccdffdf0bfbcb5e6cda3c73645c1ce903a7

--- containers/tff_worker/testing/generate_test_computations.py
+++ containers/tff_worker/testing/generate_test_computations.py
@@ -11,31 +11,83 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Binary for generating a serialized TFF computations for testing."""
+"""Binary for generating a serialized TFF computations for testing.
 
-import collections.abc
+Run using:
+
+  python3 -m venv venv && source venv/bin/activate
+  pip install --upgrade pip
+  pip install --upgrade tensorflow-federated==0.75.0
+  python3 generate_test_computations.py
+  deactivate
+"""
+
+import collections
 import os
 
 from absl import app
 from absl import flags
 
 from google.protobuf import text_format
+import tensorflow as tf
 import tensorflow_federated as tff
 
-from containers.tff_worker.testing import test_computations
-
-OUTPUT_DIR = flags.DEFINE_string('output_dir', None, 'Output directory',
-                                 required=True)
+OUTPUT_DIR = flags.DEFINE_string('output_dir', '.', 'Output directory')
 
 CLIENT_WORK_COMPUTATION = 'client_work_computation.txtpb'
 AGGREGATION_COMPUTATION = 'aggregation_computation.txtpb'
 
 
+@tff.tensorflow.computation(
+    collections.OrderedDict(
+        [('float', tff.TensorType(tf.float32.as_numpy_dtype, shape=[None]))]),
+    tf.float32.as_numpy_dtype,
+)
+def tf_comp(
+    example: collections.OrderedDict[str, tf.Tensor], broadcasted_data: float
+) -> collections.OrderedDict[str, tf.Tensor]:
+  """Adds a broadcasted value to all floats in the example tensors."""
+  result = collections.OrderedDict()
+  for name, t in example.items():
+    result[name] = tf.add(t, broadcasted_data)
+  return result
+
+
+# Define a federated computation using tf_comp to be performed per-client.
+@tff.federated_computation(
+    tff.FederatedType(
+        collections.OrderedDict([
+            ('float',
+             tff.TensorType(tf.float32.as_numpy_dtype, shape=[None]))]),
+        tff.CLIENTS,
+    ),
+    tff.FederatedType(tf.float32.as_numpy_dtype, tff.CLIENTS, all_equal=True),
+)
+def client_work_comp(
+    example: collections.OrderedDict[str, tf.Tensor], broadcasted_data: float
+) -> collections.OrderedDict[str, tf.Tensor]:
+  """Performs `tf_comp` on each client."""
+  return tff.federated_map(tf_comp, (example, broadcasted_data))
+
+
+@tff.federated_computation(
+    tff.FederatedType(tf.int32.as_numpy_dtype, tff.SERVER),
+    tff.FederatedType(tf.int32.as_numpy_dtype, tff.CLIENTS)
+)
+def aggregation_comp(state: int, value: list[int]) -> int:
+  """Scales then sums the outputs from applying client work at clients."""
+  state_at_clients = tff.federated_broadcast(state)
+  scaled_value = tff.federated_map(
+      tff.tensorflow.computation(lambda x, y: x * y), (value, state_at_clients)
+  )
+  summed_value = tff.federated_sum(scaled_value)
+  return summed_value
+
+
 def generate_test_computations() -> None:
   """Generates serialized test computations and writes them out to files."""
   client_work_computation_text_proto = text_format.MessageToString(
-      tff.framework.serialize_computation(
-          test_computations.client_work_comp))
+      tff.framework.serialize_computation(client_work_comp))
   client_work_computation_filepath = os.path.join(
       OUTPUT_DIR.value, CLIENT_WORK_COMPUTATION)
 
@@ -43,8 +95,7 @@ def generate_test_computations() -> None:
     f.write(client_work_computation_text_proto)
 
   aggregation_computation_text_proto = text_format.MessageToString(
-      tff.framework.serialize_computation(
-          test_computations.aggregation_comp))
+      tff.framework.serialize_computation(aggregation_comp))
   aggregation_computation_filepath = os.path.join(
       OUTPUT_DIR.value, AGGREGATION_COMPUTATION)
 
