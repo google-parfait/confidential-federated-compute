google-parfait/confidential-federated-compute

commit 58dd7073e568706c9a603a2e8febd0eeb9858c3c

Remove Python tff_worker and move C++ impl to containers/.
The Python tff_worker is no longer being used and is costly to maintain
(e.g. it requires a separate build process for the container). The
remaining partial C++ implementation can be moved to the `containers`
directory for consistency with other Oak Containers enclave apps.

Unfortunately, the Python and pypi deps in the WORKSPACE file can't
(yet?) be removed because they're needed by TensorFlow and by the
tff_worker's generate_test_computations tool.

Also rename *.pbtxt to *.txtpb since the latter is the canonical
extension.

Change-Id: Ice22a72b9033eaa230de0a45d99ab015686ad1b7

scripts/bazel_build.sh
@@ -48,37 +48,17 @@ elif [ "$1" == "sanitizers" ]; then
   ${BAZELISK} test //... --config=ubsan --build_tag_filters=-noubsan --test_tag_filters=-noubsan
 elif [ "$1" == "release" ]; then
   ${BAZELISK} test //...
-  ${BAZELISK} build -c opt "${!RELEASE_TARGETS[@]}" \
-      //tff_worker/server:pipeline_transform_server_zip
-  readonly BAZEL_BIN="$(${BAZELISK} info -c opt bazel-bin)"
-
-  # The Python-based tff_worker server has a more complex Dockerfile that hasn't
-  # been ported to rules_oci. Until then, build it with Docker.
-  # Copy inputs to a temporary directory to minimize the build context sent to
-  # the Docker daemon.
-  readonly DOCKER_CONTEXT_DIR="$(mktemp -d)"
-  trap 'rm -rf -- "${DOCKER_CONTEXT_DIR}"' EXIT
-  cp "${BAZEL_BIN}/tff_worker/server/pipeline_transform_server.zip" \
-      "${DOCKER_CONTEXT_DIR}/"
-  docker build -f "${WORKSPACE_DIR}/tff_worker/server/Dockerfile" \
-      -t tff_pipeline_transfer_server "${DOCKER_CONTEXT_DIR}"
-  readonly TARGET_DIR="${WORKSPACE_DIR}/target/bazel"
-  mkdir --parents "${TARGET_DIR}"
-  "${WORKSPACE_DIR}/scripts/export_container_bundle.sh" -c tff_pipeline_transfer_server \
-      -o "${TARGET_DIR}/tff_worker_container.tar"
 
   # BINARY_OUTPUTS_DIR may be unset if this script is run manually; it'll
   # always be set during CI builds.
   if [[ -n "${BINARY_OUTPUTS_DIR}" ]]; then
+    readonly BAZEL_BIN="$(${BAZELISK} info -c opt bazel-bin)"
     for target in "${!RELEASE_TARGETS[@]}"; do
       src="${BAZEL_BIN}${target/:/\//}"
       dst="${BINARY_OUTPUTS_DIR}/${RELEASE_TARGETS[$target]}"
       mkdir --parents "$(dirname "$dst")"
       cp -f "$src" "$dst"
     done
-
-    mkdir --parents "${BINARY_OUTPUTS_DIR}/tff_worker"
-    cp "${TARGET_DIR}/tff_worker_container.tar" "${BINARY_OUTPUTS_DIR}/tff_worker/container.tar"
   fi
 else
   ${BAZELISK} test //...

BUILD
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 load("@bazel_toolchains//rules/exec_properties:exec_properties.bzl", "create_rbe_exec_properties_dict")
+load("@rules_python//python:pip.bzl", "compile_pip_requirements")
 
 platform(
     name = "remote_platform",
@@ -25,4 +26,13 @@ platform(
         os_family = "Linux",
     ),
     parents = ["@local_config_platform//:host"],
+)
+
+# This rule adds a convenient way to update the requirements file.
+compile_pip_requirements(
+    name = "requirements",
+    timeout = "long",
+    extra_args = ["--allow-unsafe"],
+    requirements_in = "requirements.in",
+    requirements_txt = "requirements.txt",
 )

WORKSPACE
@@ -80,7 +80,7 @@ load("@rules_python//python:pip.bzl", "pip_parse")
 pip_parse(
     name = "pypi_deps",
     python_interpreter_target = interpreter,
-    requirements_lock = "//tff_worker:requirements.txt",
+    requirements_lock = "//:requirements.txt",
 )
 
 # Load the starlark macro, which will define pypi dependencies.
@@ -144,10 +144,6 @@ tf_workspace1(with_rules_cc = False)
 load("@org_tensorflow//tensorflow:workspace0.bzl", "tf_workspace0")
 
 tf_workspace0()
-
-load("@com_github_grpc_grpc//bazel:grpc_python_deps.bzl", "grpc_python_deps")
-
-grpc_python_deps()
 
 load("@rules_proto//proto:repositories.bzl", "rules_proto_dependencies", "rules_proto_toolchains")
 

containers/tff_worker/BUILD
@@ -11,79 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-load("@rules_python//python:defs.bzl", "py_binary", "py_test")
-
-package(default_visibility = ["//:__subpackages__"])
-
-py_binary(
-    name = "pipeline_transform_server",
-    srcs = ["pipeline_transform_server.py"],
-    main = "pipeline_transform_server.py",
-    # TFF protos are not included in the version of TFF released as a python
-    # package so we import them separately from a git_repository rule.
-    deps = [
-        ":tff_transforms",
-        "@federated-compute//fcp/protos/confidentialcompute:pipeline_transform_py_grpc",
-        "@federated-compute//fcp/protos/confidentialcompute:pipeline_transform_py_proto",
-        "@federated-compute//fcp/protos/confidentialcompute:tff_worker_configuration_py_proto",
-        "@oak//oak_containers/proto:interfaces_py_grpc",
-        "@pypi_deps_tensorflow_federated//:pkg",
-        "@tensorflow-federated//tensorflow_federated/proto/v0:computation_py_pb2",
-        "@tensorflow-federated//tensorflow_federated/proto/v0:executor_py_pb2",
-    ],
-)
-
-filegroup(
-    name = "pipeline_transform_server_zip",
-    srcs = [":pipeline_transform_server"],
-    output_group = "python_zip_file",
-    tags = ["manual"],
-)
-
-py_test(
-    name = "pipeline_transform_server_test",
-    srcs = ["pipeline_transform_server_test.py"],
-    tags = ["no-remote-exec"],  # https://github.com/bazelbuild/rules_python/issues/571
-    deps = [
-        ":pipeline_transform_server",
-        ":tff_transforms",
-        "//tff_worker/server/testing:checkpoint_test_utils",
-        "//tff_worker/server/testing:test_computations",
-        "@federated-compute//fcp/protos/confidentialcompute:pipeline_transform_py_grpc",
-        "@federated-compute//fcp/protos/confidentialcompute:pipeline_transform_py_proto",
-        "@federated-compute//fcp/protos/confidentialcompute:tff_worker_configuration_py_proto",
-        "@pypi_deps_portpicker//:pkg",
-        "@pypi_deps_tensorflow_federated//:pkg",
-    ],
-)
-
-py_library(
-    name = "tff_transforms",
-    srcs = ["tff_transforms.py"],
-    deps = [
-        "@pypi_deps_tensorflow_federated//:pkg",
-        "@tensorflow-federated//tensorflow_federated/proto/v0:computation_py_pb2",
-        "@tensorflow-federated//tensorflow_federated/proto/v0:executor_py_pb2",
-    ],
-)
-
-py_test(
-    name = "tff_transforms_test",
-    srcs = ["tff_transforms_test.py"],
-    tags = ["no-remote-exec"],  # https://github.com/bazelbuild/rules_python/issues/571
-    deps = [
-        ":tff_transforms",
-        "//tff_worker/server/testing:checkpoint_test_utils",
-        "@federated-compute//fcp/protos/confidentialcompute:tff_worker_configuration_py_proto",
-        "@org_tensorflow//tensorflow/core:protos_all_py",
-        "@pypi_deps_tensorflow_federated//:pkg",
-        "@tensorflow-federated//tensorflow_federated/proto/v0:computation_py_pb2",
-        "@tensorflow-federated//tensorflow_federated/proto/v0:executor_py_pb2",
-    ],
-)
 
 cc_library(
-    name = "cpp_pipeline_transform_server",
+    name = "pipeline_transform_server",
     srcs = ["pipeline_transform_server.cc"],
     hdrs = ["pipeline_transform_server.h"],
     deps = [
@@ -110,12 +40,12 @@ cc_library(
 )
 
 cc_test(
-    name = "cpp_pipeline_transform_server_test",
+    name = "pipeline_transform_server_test",
     size = "small",
     srcs = ["pipeline_transform_server_test.cc"],
     data = [
-        "//tff_worker/server/testing:aggregation_computation.pbtxt",
-        "//tff_worker/server/testing:client_work_computation.pbtxt",
+        "//containers/tff_worker/testing:aggregation_computation.txtpb",
+        "//containers/tff_worker/testing:client_work_computation.txtpb",
     ],
     tags = [
         # There is an incompatibility between the TensorFlow dependencies for
@@ -125,7 +55,7 @@ cc_test(
         "noubsan",
     ],
     deps = [
-        ":cpp_pipeline_transform_server",
+        ":pipeline_transform_server",
         "@com_github_grpc_grpc//:grpc++",
         "@com_google_absl//absl/log",
         "@com_google_absl//absl/status",

containers/tff_worker/pipeline_transform_server.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "tff_worker/server/pipeline_transform_server.h"
+#include "containers/tff_worker/pipeline_transform_server.h"
 
 #include <string>
 

containers/tff_worker/pipeline_transform_server.h
@@ -14,8 +14,8 @@
  * limitations under the License.
  */
 
-#ifndef CONFIDENTIAL_FEDERATED_COMPUTE_TFF_WORKER_SERVER_PIPELINE_TRANSFORM_SERVER_H_
-#define CONFIDENTIAL_FEDERATED_COMPUTE_TFF_WORKER_SERVER_PIPELINE_TRANSFORM_SERVER_H_
+#ifndef CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_TFF_WORKER_PIPELINE_TRANSFORM_SERVER_H_
+#define CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_TFF_WORKER_PIPELINE_TRANSFORM_SERVER_H_
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
@@ -59,4 +59,4 @@ class TffPipelineTransform final
 
 }  // namespace confidential_federated_compute::tff_worker
 
-#endif  // CONFIDENTIAL_FEDERATED_COMPUTE_TFF_WORKER_SERVER_PIPELINE_TRANSFORM_SERVER_H_
+#endif  // CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_TFF_WORKER_PIPELINE_TRANSFORM_SERVER_H_

containers/tff_worker/pipeline_transform_server_test.cc
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#include "tff_worker/server/pipeline_transform_server.h"
+#include "containers/tff_worker/pipeline_transform_server.h"
 
 #include <fstream>
 #include <memory>
@@ -82,9 +82,9 @@ using ::testing::HasSubstr;
 using ::testing::Test;
 
 constexpr absl::string_view kAggregationComputationPath =
-    "tff_worker/server/testing/aggregation_computation.pbtxt";
+    "containers/tff_worker/testing/aggregation_computation.txtpb";
 constexpr absl::string_view kClientWorkComputationPath =
-    "tff_worker/server/testing/client_work_computation.pbtxt";
+    "containers/tff_worker/testing/client_work_computation.txtpb";
 
 absl::StatusOr<tff_proto::Computation> LoadFileAsTffComputation(
     absl::string_view path) {

containers/tff_worker/testing/BUILD
@@ -11,14 +11,31 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+load("@rules_python//python:defs.bzl", "py_binary")
+
 package(default_visibility = ["//:__subpackages__"])
 
-cc_binary(
-    name = "pipeline_transform_cpp_client",
-    srcs = ["pipeline_transform_client.cc"],
+exports_files([
+    "aggregation_computation.txtpb",
+    "client_work_computation.txtpb",
+])
+
+py_library(
+    name = "test_computations",
+    testonly = True,
+    srcs = ["test_computations.py"],
     deps = [
-        "@com_github_grpc_grpc//:grpc++",
-        "@federated-compute//fcp/protos/confidentialcompute:pipeline_transform_cc_grpc",
-        "@federated-compute//fcp/protos/confidentialcompute:pipeline_transform_cc_proto",
+        "@pypi_deps_tensorflow//:pkg",
+        "@pypi_deps_tensorflow_federated//:pkg",
+    ],
+)
+
+py_binary(
+    name = "generate_test_computations",
+    testonly = True,
+    srcs = ["generate_test_computations.py"],
+    deps = [
+        ":test_computations",
+        "@pypi_deps_tensorflow_federated//:pkg",
     ],
 )

commit 7185ef5817cf2407019b380913586c2075ceb75c

Fix broken release builds.
58dd7073e568706c9a603a2e8febd0eeb9858c3c accidentally removed the
release binary build when removing the python tff_worker.

Change-Id: I5e7a9116ff92e6df4e800010b809df65eb8412a0

scripts/bazel_build.sh
@@ -48,6 +48,7 @@ elif [ "$1" == "sanitizers" ]; then
   ${BAZELISK} test //... --config=ubsan --build_tag_filters=-noubsan --test_tag_filters=-noubsan
 elif [ "$1" == "release" ]; then
   ${BAZELISK} test //...
+  ${BAZELISK} build -c opt "${!RELEASE_TARGETS[@]}"
 
   # BINARY_OUTPUTS_DIR may be unset if this script is run manually; it'll
   # always be set during CI builds.