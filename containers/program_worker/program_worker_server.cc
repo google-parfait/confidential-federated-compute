a1ac652ad5bd5be064a3137f1c6d40460358c80f
Yu Xiao <xiaoyux@google.com>
2025-03-06 11:47:59 -0800

    Implement the Execute handler for program worker TEE.
    Now we assume that the function and arg for the computation are both tff values, and we will return the tff value result directly, without considering the data pointer.
    
    Change-Id: I3c21be813dc42e1dafef05b7ef9d992378ecf677

--- containers/program_worker/program_worker_server.cc
+++ containers/program_worker/program_worker_server.cc
@@ -13,23 +13,111 @@
 // limitations under the License.
 #include "containers/program_worker/program_worker_server.h"
 
+#include "absl/status/statusor.h"
 #include "fcp/base/status_converters.h"
+#include "fcp/confidentialcompute/tff_execution_helper.h"
 #include "fcp/protos/confidentialcompute/program_worker.grpc.pb.h"
 #include "fcp/protos/confidentialcompute/program_worker.pb.h"
+#include "fcp/protos/confidentialcompute/tff_config.pb.h"
+#include "tensorflow_federated/cc/core/impl/executors/executor.h"
+#include "tensorflow_federated/cc/core/impl/executors/federating_executor.h"
+#include "tensorflow_federated/cc/core/impl/executors/reference_resolving_executor.h"
+#include "tensorflow_federated/cc/core/impl/executors/tensorflow_executor.h"
+#include "tensorflow_federated/proto/v0/executor.pb.h"
 
 namespace confidential_federated_compute::program_worker {
 
 using ::fcp::base::ToGrpcStatus;
 using ::fcp::confidentialcompute::ComputationRequest;
 using ::fcp::confidentialcompute::ComputationResponse;
+using ::fcp::confidentialcompute::TffSessionConfig;
 using ::grpc::ServerContext;
 using ::grpc::Status;
+
+namespace {
+
+absl::StatusOr<std::shared_ptr<tensorflow_federated::Executor>>
+InitializeTffExecutor(const TffSessionConfig& comp_request) {
+  int32_t max_concurrent_computation_calls =
+      comp_request.max_concurrent_computation_calls();
+  auto leaf_executor_fn = [max_concurrent_computation_calls]() {
+    return tensorflow_federated::CreateReferenceResolvingExecutor(
+        tensorflow_federated::CreateTensorFlowExecutor(
+            max_concurrent_computation_calls));
+  };
+  tensorflow_federated::CardinalityMap cardinality_map;
+  cardinality_map[tensorflow_federated::kClientsUri] =
+      comp_request.num_clients();
+  FCP_ASSIGN_OR_RETURN(
+      auto federating_executor,
+      tensorflow_federated::CreateFederatingExecutor(
+          /*server_child=*/leaf_executor_fn(),
+          /*client_child=*/leaf_executor_fn(), cardinality_map));
+  return tensorflow_federated::CreateReferenceResolvingExecutor(
+      federating_executor);
+}
+
+}  // namespace
 
 grpc::Status ProgramWorkerTee::Execute(ServerContext* context,
                                        const ComputationRequest* request,
                                        ComputationResponse* response) {
-  // TODO - b/378243349: Add implementation based on the computation type.
-  return ToGrpcStatus(absl::UnimplementedError("Not implemented"));
+  // We now use the TffSessionConfig as the request message.
+  TffSessionConfig comp_request;
+  if (!request->computation().UnpackTo(&comp_request)) {
+    return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT,
+                        "ComputationRequest invalid.");
+  }
+
+  // Initialize the TFF executor according to the TffSessionConfig embedded in
+  // the request.
+  auto child_executor_or = InitializeTffExecutor(comp_request);
+  if (!child_executor_or.ok()) {
+    return ToGrpcStatus(child_executor_or.status());
+  }
+  std::shared_ptr<tensorflow_federated::Executor> child_executor =
+      std::move(child_executor_or.value());
+
+  tensorflow_federated::v0::Value function = std::move(comp_request.function());
+  std::optional<tensorflow_federated::v0::Value> argument = std::nullopt;
+  if (comp_request.has_initial_arg()) {
+    argument = std::move(comp_request.initial_arg());
+  }
+  // TODO: b/378243349 - Add support for data pointers input.
+  // We now assume that the function and argument are all tff values.
+  auto fn_handle_or = child_executor->CreateValue(function);
+  if (!fn_handle_or.ok()) {
+    return ToGrpcStatus(fn_handle_or.status());
+  }
+
+  std::optional<tensorflow_federated::OwnedValueId> optional_arg_handle;
+  if (argument.has_value()) {
+    auto arg_handle =
+        fcp::confidential_compute::Embed(argument.value(), child_executor);
+    if (!arg_handle.ok()) {
+      return ToGrpcStatus(arg_handle.status());
+    }
+    optional_arg_handle = std::move(*arg_handle.value());
+  }
+
+  auto call_handle_or =
+      child_executor->CreateCall(fn_handle_or.value(), optional_arg_handle);
+  if (!call_handle_or.ok()) {
+    return ToGrpcStatus(call_handle_or.status());
+  }
+  tensorflow_federated::OwnedValueId call_handle =
+      std::move(call_handle_or.value());
+  tensorflow_federated::v0::Value call_result;
+  auto materialize_status =
+      child_executor->Materialize(call_handle, &call_result);
+  if (!materialize_status.ok()) {
+    return ToGrpcStatus(materialize_status);
+  }
+  // TODO b/378243349 - Encrypt the result into a blob with the
+  // output_access_policy_node_id set in the request.
+  response->mutable_result()->PackFrom(call_result);
+
+  return grpc::Status::OK;
 }
 
 }  // namespace confidential_federated_compute::program_worker
--- containers/program_worker/program_worker_server_test.cc
+++ containers/program_worker/program_worker_server_test.cc
@@ -13,6 +13,7 @@
 // limitations under the License.
 #include "containers/program_worker/program_worker_server.h"
 
+#include <fstream>
 #include <memory>
 #include <string>
 
@@ -20,8 +21,10 @@
 #include "absl/log/log.h"
 #include "fcp/protos/confidentialcompute/program_worker.grpc.pb.h"
 #include "fcp/protos/confidentialcompute/program_worker.pb.h"
+#include "fcp/protos/confidentialcompute/tff_config.pb.h"
 #include "gmock/gmock.h"
 #include "google/protobuf/repeated_ptr_field.h"
+#include "google/protobuf/struct.pb.h"
 #include "grpcpp/channel.h"
 #include "grpcpp/client_context.h"
 #include "grpcpp/create_channel.h"
@@ -30,6 +33,7 @@
 #include "grpcpp/server_context.h"
 #include "gtest/gtest.h"
 #include "proto/containers/orchestrator_crypto_mock.grpc.pb.h"
+#include "testing/parse_text_proto.h"
 
 namespace confidential_federated_compute::program_worker {
 
@@ -38,12 +42,57 @@ namespace {
 using ::fcp::confidentialcompute::ComputationRequest;
 using ::fcp::confidentialcompute::ComputationResponse;
 using ::fcp::confidentialcompute::ProgramWorker;
+using ::fcp::confidentialcompute::TffSessionConfig;
 using ::grpc::Server;
 using ::grpc::ServerBuilder;
 using ::grpc::ServerContext;
 using ::grpc::StatusCode;
 using ::oak::containers::v1::MockOrchestratorCryptoStub;
+using ::tensorflow_federated::v0::Value;
 using ::testing::Test;
+
+constexpr absl::string_view kNoArgumentComputationPath =
+    "containers/program_worker/testing/no_argument_comp.txtpb";
+constexpr absl::string_view kNoArgumentComputationExpectedResultPath =
+    "containers/program_worker/testing/no_argument_comp_expected_result.txtpb";
+constexpr absl::string_view kServerDataCompPath =
+    "containers/program_worker/testing/server_data_comp.txtpb";
+constexpr absl::string_view kServerDataPath =
+    "containers/program_worker/testing/server_data.txtpb";
+constexpr absl::string_view kServerDataCompExpectedResultPath =
+    "containers/program_worker/testing/server_data_comp_expected_result.txtpb";
+
+absl::StatusOr<Value> LoadFileAsTffValue(absl::string_view path,
+                                         bool is_computation = true) {
+  // Before creating the std::ifstream, convert the absl::string_view to
+  // std::string.
+  std::string path_str(path);
+  std::ifstream file_istream(path_str);
+  if (!file_istream) {
+    return absl::FailedPreconditionError("Error loading file: " + path_str);
+  }
+  std::stringstream file_stream;
+  file_stream << file_istream.rdbuf();
+  if (is_computation) {
+    federated_language::Computation computation;
+    if (!google::protobuf::TextFormat::ParseFromString(
+            std::move(file_stream.str()), &computation)) {
+      return absl::InvalidArgumentError(
+          "Error parsing TFF Computation from file.");
+    }
+    Value value;
+    *value.mutable_computation() = std::move(computation);
+    return value;
+  } else {
+    Value value;
+    if (!google::protobuf::TextFormat::ParseFromString(
+            std::move(file_stream.str()), &value)) {
+      return absl::InvalidArgumentError(
+          "Error parsing TFF Federated from file.");
+    }
+    return value;
+  }
+}
 
 class ProgramWorkerTeeServerTest : public Test {
  public:
@@ -62,7 +111,12 @@ class ProgramWorkerTeeServerTest : public Test {
                             grpc::InsecureChannelCredentials()));
   }
 
-  ~ProgramWorkerTeeServerTest() override { server_->Shutdown(); }
+  ~ProgramWorkerTeeServerTest() override {
+    if (server_ != nullptr) {
+      server_->Shutdown();
+      server_->Wait();
+    }
+  }
 
  protected:
   testing::NiceMock<MockOrchestratorCryptoStub> mock_crypto_stub_;
@@ -71,13 +125,68 @@ class ProgramWorkerTeeServerTest : public Test {
   std::unique_ptr<ProgramWorker::Stub> stub_;
 };
 
-TEST_F(ProgramWorkerTeeServerTest, ExecuteReturnsUnimplemented) {
+TEST_F(ProgramWorkerTeeServerTest, ExecuteReturnsInvalidArgumentError) {
   grpc::ClientContext context;
   ComputationRequest request;
+  google::protobuf::Value string_value;
+  string_value.set_string_value("test");
+  request.mutable_computation()->PackFrom(string_value);
   ComputationResponse response;
 
   auto status = stub_->Execute(&context, request, &response);
-  ASSERT_EQ(status.error_code(), grpc::StatusCode::UNIMPLEMENTED);
+  ASSERT_EQ(status.error_code(), grpc::StatusCode::INVALID_ARGUMENT);
+}
+
+TEST_F(ProgramWorkerTeeServerTest, ExecuteNoArgumentComputationReturnsResult) {
+  grpc::ClientContext context;
+  ComputationRequest request;
+
+  TffSessionConfig comp_request;
+  auto function = LoadFileAsTffValue(kNoArgumentComputationPath);
+  ASSERT_TRUE(function.ok());
+  *comp_request.mutable_function() = *function;
+  comp_request.set_num_clients(3);
+  comp_request.set_output_access_policy_node_id(1);
+  comp_request.set_max_concurrent_computation_calls(1);
+  request.mutable_computation()->PackFrom(comp_request);
+  ComputationResponse response;
+
+  auto status = stub_->Execute(&context, request, &response);
+  ASSERT_TRUE(status.ok());
+
+  Value result;
+  ASSERT_TRUE(response.result().UnpackTo(&result));
+  auto expected_result =
+      LoadFileAsTffValue(kNoArgumentComputationExpectedResultPath, false);
+  ASSERT_TRUE(expected_result.ok());
+  ASSERT_EQ(result.SerializeAsString(), expected_result->SerializeAsString());
+}
+
+TEST_F(ProgramWorkerTeeServerTest, ExecuteServerDataCompReturnsResult) {
+  grpc::ClientContext context;
+  ComputationRequest request;
+  TffSessionConfig comp_request;
+  auto function = LoadFileAsTffValue(kServerDataCompPath);
+  ASSERT_TRUE(function.ok());
+  *comp_request.mutable_function() = *function;
+  auto arg = LoadFileAsTffValue(kServerDataPath, false);
+  ASSERT_TRUE(arg.ok());
+  *comp_request.mutable_initial_arg() = *arg;
+  comp_request.set_num_clients(3);
+  comp_request.set_output_access_policy_node_id(1);
+  comp_request.set_max_concurrent_computation_calls(1);
+  request.mutable_computation()->PackFrom(comp_request);
+  ComputationResponse response;
+
+  auto status = stub_->Execute(&context, request, &response);
+  ASSERT_TRUE(status.ok());
+
+  Value result;
+  ASSERT_TRUE(response.result().UnpackTo(&result));
+  auto expected_result =
+      LoadFileAsTffValue(kServerDataCompExpectedResultPath, false);
+  ASSERT_TRUE(expected_result.ok());
+  ASSERT_EQ(result.SerializeAsString(), expected_result->SerializeAsString());
 }
 
 }  // namespace
