228bac620505f9dc660053457af8728e24e21a38
Katharine Daly <dalyk@google.com>
2025-03-14 18:15:14 +0000

    Add ability to execute a simple hardcoded federated program from C++.
    This cl introduces two layers of pybind:
    - C++ confidential_transform_server <-> python program_runner
    - python execution_context <-> C++ computation_runner
    
    Finding versions of the pip dependencies needed by federated_language
    and org_tensorflow_federated that didn't result in errors when used
    alongside these two layers of pybind was tricky. It appears that
    dm-tree==0.1.7 must be used, which forces some downstream changes to
    the versions of other dependencies. It did not seem possible to find a
    compatible version of dp_accounting so a patch was introduced to remove
    this dependency from TFF. Once we no longer need to depend on TF, the
    pip dependency situation should become easier to manage.
    
    This cl is meant to establish the initial code needed for executing
    a python federated program from C++ and prove that there is a suitable
    set of dependencies that works. It uses a hardcoded program and
    hardcoded DataReadWrite service port for now. Also, only non-distributed
    TF execution is supported in the execution stack for now. A basic
    ReleaseManager allows results produced within the federated program to
    be exported to untrusted space.
    
    In follow-up cls, the program and DataReadWrite service port will become
    customizable and the execution context/stack will become more flexible
    (supporting distributed execution and XLA).
    
    Change-Id: I918eff423b0664d49dc975dc8d1b9e8771863f8f

---
+++ containers/program_executor_tee/program_context/cc/computation_runner.h
@@ -0,0 +1,48 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_PROGRAM_EXECUTOR_TEE_PROGRAM_CONTEXT_CC_COMPUTATION_RUNNER_H_
+#define CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_PROGRAM_EXECUTOR_TEE_PROGRAM_CONTEXT_CC_COMPUTATION_RUNNER_H_
+
+#include <optional>
+
+#include "absl/status/statusor.h"
+#include "tensorflow_federated/proto/v0/executor.pb.h"
+
+namespace confidential_federated_compute::program_executor_tee {
+
+// Stateful helper class for executing TFF computations.
+class ComputationRunner {
+ public:
+  ComputationRunner(std::vector<std::string> worker_bns)
+      : worker_bns_(worker_bns) {}
+
+  // Executes a TFF computation using a C++ execution stack. If worker_bns_ is
+  // empty, the computation will be executed in a non-distributed manner, else
+  // parts of the computation will be distributed to the workers. Only TF is
+  // supported for now.
+  // TODO: Add support for XLA execution.
+  absl::StatusOr<tensorflow_federated::v0::Value> InvokeComp(
+      int num_clients, tensorflow_federated::v0::Value comp,
+      std::optional<tensorflow_federated::v0::Value> arg);
+
+ private:
+  // Addresses of worker machines running the program_worker binary that can be
+  // used to execute computations in a distributed manner.
+  const std::vector<std::string> worker_bns_;
+};
+
+}  // namespace confidential_federated_compute::program_executor_tee
+
+#endif  // CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_PROGRAM_EXECUTOR_TEE_PROGRAM_CONTEXT_CC_COMPUTATION_RUNNER_H_
