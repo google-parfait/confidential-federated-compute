46cb720eedaebe555b638e60aa2015bc72096ffc
Stanislav Chiknavaryan <stanisc@google.com>
2025-04-22 19:17:27 +0000

    Change IntervalSet to disallow adding or merging overlapping intervals
    Change-Id: Iea4998d887ec57f0062270885cd3c272c27eb2a6

--- containers/fed_sql/interval_set_test.cc
+++ containers/fed_sql/interval_set_test.cc
@@ -14,6 +14,8 @@
 
 #include "containers/fed_sql/interval_set.h"
 
+#include <initializer_list>
+
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 
@@ -40,6 +42,27 @@ TEST(IntervalSetTest, OneInterval) {
   EXPECT_TRUE(set.Contains(4));
 }
 
+TEST(IntervalSet, Assign) {
+  IntervalSet<int> set({{1, 5}});
+  std::initializer_list<Interval<int>> intervals = {{0, 1}, {2, 3}};
+  EXPECT_TRUE(set.Assign(intervals.begin(), intervals.end()));
+  EXPECT_THAT(set, ElementsAre(Interval(0, 1), Interval(2, 3)));
+}
+
+TEST(IntervalSet, AssignUnordered) {
+  IntervalSet<int> set({{1, 5}});
+  std::initializer_list<Interval<int>> intervals = {{2, 3}, {0, 1}};
+  EXPECT_FALSE(set.Assign(intervals.begin(), intervals.end()));
+  EXPECT_THAT(set, ElementsAre(Interval(1, 5)));
+}
+
+TEST(IntervalSet, AssignNotDisjoint) {
+  IntervalSet<int> set({{1, 5}});
+  std::initializer_list<Interval<int>> intervals = {{0, 3}, {3, 4}};
+  EXPECT_FALSE(set.Assign(intervals.begin(), intervals.end()));
+  EXPECT_THAT(set, ElementsAre(Interval(1, 5)));
+}
+
 TEST(IntervalSetTest, TwoIntervals) {
   IntervalSet<int> set({{1, 3}, {6, 9}});
   EXPECT_EQ(set.size(), 2);
@@ -53,84 +76,107 @@ TEST(IntervalSetTest, TwoIntervals) {
 
 TEST(IntervalSetTest, EmptyIntervalsIgnored) {
   IntervalSet<int> set;
-  set.Add(Interval<int>());
-  set.Add({10, 10});
+  EXPECT_TRUE(set.Add(Interval<int>()));
+  EXPECT_TRUE(set.Add({10, 10}));
   EXPECT_THAT(set, ElementsAre());
 }
 
 TEST(IntervalSetTest, AddMultiple) {
   IntervalSet<int> set;
-  set.Add({3, 4});
-  set.Add({5, 6});
-  set.Add({1, 2});
-  set.Add({10, 11});
-  EXPECT_THAT(set, ElementsAre(Interval(1, 2), Interval(3, 4), Interval(5, 6),
-                               Interval(10, 11)));
-
-  set.Add({11, 12});
-  EXPECT_THAT(set, ElementsAre(Interval(1, 2), Interval(3, 4), Interval(5, 6),
-                               Interval(10, 12)));
-
-  set.Add({4, 5});
+  EXPECT_TRUE(set.Add({3, 4}));
+  EXPECT_TRUE(set.Add({5, 6}));
+  EXPECT_TRUE(set.Add({11, 12}));
   EXPECT_THAT(set,
-              ElementsAre(Interval(1, 2), Interval(3, 6), Interval(10, 12)));
+              ElementsAre(Interval(3, 4), Interval(5, 6), Interval(11, 12)));
 
-  set.Add({7, 8});
-  EXPECT_THAT(set, ElementsAre(Interval(1, 2), Interval(3, 6), Interval(7, 8),
-                               Interval(10, 12)));
+  EXPECT_TRUE(set.Add({1, 2}));
+  EXPECT_THAT(set, ElementsAre(Interval(1, 2), Interval(3, 4), Interval(5, 6),
+                               Interval(11, 12)));
 
-  set.Add({5, 9});
+  EXPECT_TRUE(set.Add({0, 1}));
+  EXPECT_THAT(set, ElementsAre(Interval(0, 2), Interval(3, 4), Interval(5, 6),
+                               Interval(11, 12)));
+
+  EXPECT_TRUE(set.Add({8, 9}));
+  EXPECT_THAT(set, ElementsAre(Interval(0, 2), Interval(3, 4), Interval(5, 6),
+                               Interval(8, 9), Interval(11, 12)));
+
+  EXPECT_TRUE(set.Add({6, 7}));
+  EXPECT_THAT(set, ElementsAre(Interval(0, 2), Interval(3, 4), Interval(5, 7),
+                               Interval(8, 9), Interval(11, 12)));
+
+  EXPECT_TRUE(set.Add({12, 15}));
+  EXPECT_THAT(set, ElementsAre(Interval(0, 2), Interval(3, 4), Interval(5, 7),
+                               Interval(8, 9), Interval(11, 15)));
+
+  EXPECT_TRUE(set.Add({10, 11}));
+  EXPECT_THAT(set, ElementsAre(Interval(0, 2), Interval(3, 4), Interval(5, 7),
+                               Interval(8, 9), Interval(10, 15)));
+
+  EXPECT_TRUE(set.Add({2, 3}));
+  EXPECT_THAT(set, ElementsAre(Interval(0, 4), Interval(5, 7), Interval(8, 9),
+                               Interval(10, 15)));
+
+  EXPECT_TRUE(set.Add({7, 8}));
   EXPECT_THAT(set,
-              ElementsAre(Interval(1, 2), Interval(3, 9), Interval(10, 12)));
+              ElementsAre(Interval(0, 4), Interval(5, 9), Interval(10, 15)));
 
-  set.Add({1, 15});
-  EXPECT_THAT(set, ElementsAre(Interval(1, 15)));
+  EXPECT_TRUE(set.Add({9, 10}));
+  EXPECT_THAT(set, ElementsAre(Interval(0, 4), Interval(5, 15)));
 
-  set.Add({0, 1});
+  EXPECT_TRUE(set.Add({4, 5}));
   EXPECT_THAT(set, ElementsAre(Interval(0, 15)));
+}
 
-  set.Add({20, 22});
-  EXPECT_THAT(set, ElementsAre(Interval(0, 15), Interval(20, 22)));
-
-  set.Add({-2, 10});
-  EXPECT_THAT(set, ElementsAre(Interval(-2, 15), Interval(20, 22)));
-
-  set.Add({17, 21});
-  EXPECT_THAT(set, ElementsAre(Interval(-2, 15), Interval(17, 22)));
-
-  set.Add({10, 20});
-  EXPECT_THAT(set, ElementsAre(Interval(-2, 22)));
+TEST(IntervalSetTest, AddOverlapping) {
+  IntervalSet<int> set({{1, 3}, {6, 9}});
+  EXPECT_FALSE(set.Add({0, 2}));
+  EXPECT_FALSE(set.Add({0, 4}));
+  EXPECT_FALSE(set.Add({2, 6}));
+  EXPECT_FALSE(set.Add({3, 7}));
+  EXPECT_FALSE(set.Add({6, 9}));
+  EXPECT_FALSE(set.Add({7, 9}));
+  EXPECT_FALSE(set.Add({8, 10}));
+  EXPECT_THAT(set, ElementsAre(Interval(1, 3), Interval(6, 9)));
 }
 
 TEST(IntervalSetTest, Merge) {
   IntervalSet<int> set1(
       {{2, 4}, {6, 8}, {12, 13}, {14, 15}, {16, 18}, {28, 36}});
-  IntervalSet<int> set2({{0, 1}, {3, 5}, {7, 9}, {10, 22}, {24, 26}, {30, 35}});
+  IntervalSet<int> set2(
+      {{0, 1}, {4, 5}, {10, 12}, {15, 16}, {18, 28}, {40, 45}});
 
-  set1.Merge(set2);
+  EXPECT_TRUE(set1.Merge(set2));
   EXPECT_THAT(
-      set1, ElementsAre(Interval(0, 1), Interval(2, 5), Interval(6, 9),
-                        Interval(10, 22), Interval(24, 26), Interval(28, 36)));
+      set1, ElementsAre(Interval(0, 1), Interval(2, 5), Interval(6, 8),
+                        Interval(10, 13), Interval(14, 36), Interval(40, 45)));
 
   IntervalSet<int> set3({{0, 1}, {2, 3}, {4, 5}, {6, 7}});
   IntervalSet<int> set4({{1, 2}, {3, 4}, {5, 6}, {7, 8}});
-  set3.Merge(set4);
+  EXPECT_TRUE(set3.Merge(set4));
   EXPECT_THAT(set3, ElementsAre(Interval(0, 8)));
 }
 
 TEST(IntervalSetTest, MergeWithEmptySet) {
   IntervalSet<int> set1;
   IntervalSet<int> set2;
-  set1.Merge(set2);
+  EXPECT_TRUE(set1.Merge(set2));
   EXPECT_THAT(set1, ElementsAre());
 
   IntervalSet<int> set3({{1, 2}});
-  set1.Merge(set3);
+  EXPECT_TRUE(set1.Merge(set3));
   EXPECT_THAT(set1, ElementsAre(Interval(1, 2)));
 
   IntervalSet<int> set4;
-  set1.Merge(set4);
+  EXPECT_TRUE(set1.Merge(set4));
   EXPECT_THAT(set1, ElementsAre(Interval(1, 2)));
+}
+
+TEST(IntervalSetTest, MergeOverlapping) {
+  IntervalSet<int> set1({{2, 4}});
+  IntervalSet<int> set2({{3, 5}});
+  EXPECT_FALSE(set1.Merge(set2));
+  EXPECT_THAT(set1, ElementsAre(Interval(2, 4)));
 }
 
 }  // namespace
