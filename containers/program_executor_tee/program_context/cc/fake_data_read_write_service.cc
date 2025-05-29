e56cf386afa677e63238ec1191a48ae13dbd01b5
Katharine Daly <dalyk@google.com>
2025-05-29 05:11:30 +0000

    Add functionality to DataReadWriteService to support ReadRequests.
    Also add tests.
    
    Change-Id: Iffb68c855440a659862637d38733e9c243ffd737

--- containers/program_executor_tee/program_context/cc/fake_data_read_write_service.cc
+++ containers/program_executor_tee/program_context/cc/fake_data_read_write_service.cc
@@ -14,15 +14,44 @@
 
 #include "containers/program_executor_tee/program_context/cc/fake_data_read_write_service.h"
 
+#include <map>
+#include <string>
+#include <vector>
+
+#include "absl/log/log.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "containers/blob_metadata.h"
+#include "containers/crypto_test_utils.h"
+#include "fcp/base/monitoring.h"
 #include "fcp/protos/confidentialcompute/data_read_write.pb.h"
+#include "fcp/protos/confidentialcompute/pipeline_transform.pb.h"
 #include "grpcpp/grpcpp.h"
 #include "grpcpp/server_context.h"
 #include "grpcpp/support/sync_stream.h"
 
 namespace confidential_federated_compute::program_executor_tee {
 
+using ::fcp::confidentialcompute::Record;
+using ::fcp::confidentialcompute::outgoing::ReadRequest;
+using ::fcp::confidentialcompute::outgoing::ReadResponse;
 using ::fcp::confidentialcompute::outgoing::WriteRequest;
 using ::fcp::confidentialcompute::outgoing::WriteResponse;
+
+grpc::Status FakeDataReadWriteService::Read(
+    ::grpc::ServerContext*, const ReadRequest* request,
+    grpc::ServerWriter<ReadResponse>* response_writer) {
+  if (uri_to_read_response_.find(request->uri()) ==
+      uri_to_read_response_.end()) {
+    return grpc::Status(grpc::StatusCode::NOT_FOUND,
+                        "Requested uri " + request->uri() + " not found.");
+  }
+  response_writer->Write(uri_to_read_response_[request->uri()]);
+  // Store the uri from the request.
+  read_request_uris_.push_back(request->uri());
+  return grpc::Status::OK;
+}
 
 grpc::Status FakeDataReadWriteService::Write(
     ::grpc::ServerContext*, ::grpc::ServerReader<WriteRequest>* request_reader,
@@ -35,6 +64,57 @@ grpc::Status FakeDataReadWriteService::Write(
   }
   write_call_args_.push_back(requests);
   return grpc::Status::OK;
+}
+
+absl::Status FakeDataReadWriteService::StoreEncryptedMessage(
+    absl::string_view uri, absl::string_view message,
+    absl::string_view ciphertext_associated_data,
+    absl::string_view recipient_public_key, absl::string_view nonce,
+    absl::string_view reencryption_public_key) {
+  if (uri_to_read_response_.find(std::string(uri)) !=
+      uri_to_read_response_.end()) {
+    return absl::InvalidArgumentError("Uri already set.");
+  }
+
+  FCP_ASSIGN_OR_RETURN(
+      absl::StatusOr<Record> rewrapped_record,
+      crypto_test_utils::CreateRewrappedRecord(
+          message, ciphertext_associated_data, recipient_public_key, nonce,
+          reencryption_public_key));
+
+  ReadResponse response;
+  *response.mutable_first_response_metadata() =
+      GetBlobMetadataFromRecord(*rewrapped_record);
+  response.set_finish_read(true);
+  response.set_data(rewrapped_record->hpke_plus_aead_data().ciphertext());
+
+  uri_to_read_response_[std::string(uri)] = std::move(response);
+  return absl::OkStatus();
+}
+
+absl::Status FakeDataReadWriteService::StorePlaintextMessage(
+    absl::string_view uri, absl::string_view message) {
+  if (uri_to_read_response_.find(std::string(uri)) !=
+      uri_to_read_response_.end()) {
+    return absl::InvalidArgumentError("Uri already set.");
+  }
+
+  Record record;
+  record.set_unencrypted_data(std::string(message));
+  record.set_compression_type(Record::COMPRESSION_TYPE_NONE);
+
+  ReadResponse response;
+  *response.mutable_first_response_metadata() =
+      GetBlobMetadataFromRecord(record);
+  response.set_finish_read(true);
+  response.set_data(record.unencrypted_data());
+
+  uri_to_read_response_[std::string(uri)] = std::move(response);
+  return absl::OkStatus();
+}
+
+std::vector<std::string> FakeDataReadWriteService::GetReadRequestUris() {
+  return read_request_uris_;
 }
 
 std::vector<std::vector<WriteRequest>>
--- containers/program_executor_tee/program_context/cc/fake_data_read_write_service.h
+++ containers/program_executor_tee/program_context/cc/fake_data_read_write_service.h
@@ -15,6 +15,12 @@
 #ifndef CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_PROGRAM_EXECUTOR_TEE_PROGRAM_CONTEXT_CC_FAKE_DATA_READ_WRITE_SERVICE_H_
 #define CONFIDENTIAL_FEDERATED_COMPUTE_CONTAINERS_PROGRAM_EXECUTOR_TEE_PROGRAM_CONTEXT_CC_FAKE_DATA_READ_WRITE_SERVICE_H_
 
+#include <map>
+#include <string>
+#include <vector>
+
+#include "absl/status/status.h"
+#include "absl/strings/string_view.h"
 #include "fcp/protos/confidentialcompute/data_read_write.grpc.pb.h"
 #include "fcp/protos/confidentialcompute/data_read_write.pb.h"
 #include "grpcpp/grpcpp.h"
@@ -23,21 +29,54 @@
 
 namespace confidential_federated_compute::program_executor_tee {
 
-// Fake DataReadWrite service that records WriteRequest call args.
+// Fake DataReadWrite service that retains information about past requests
+// and can send mock responses.
 class FakeDataReadWriteService
     : public fcp::confidentialcompute::outgoing::DataReadWrite::Service {
  public:
+  grpc::Status Read(
+      ::grpc::ServerContext*,
+      const ::fcp::confidentialcompute::outgoing::ReadRequest* request,
+      grpc::ServerWriter<::fcp::confidentialcompute::outgoing::ReadResponse>*
+          response_writer) override;
+
   grpc::Status Write(
       ::grpc::ServerContext*,
       ::grpc::ServerReader<fcp::confidentialcompute::outgoing::WriteRequest>*
           request_reader,
       fcp::confidentialcompute::outgoing::WriteResponse*) override;
 
+  // Encrypts the provided message and stores it within a ReadResponse in an
+  // internal map such that the ReadResponse will be returned in response to a
+  // later ReadRequest for the given uri.
+  absl::Status StoreEncryptedMessage(
+      absl::string_view uri, absl::string_view message,
+      absl::string_view ciphertext_associated_data,
+      absl::string_view recipient_public_key, absl::string_view nonce,
+      absl::string_view reencryption_public_key);
+
+  // Stores the provided message within a ReadResponse in an internal map such
+  // that the ReadResponse will be returned in response to a later ReadRequest
+  // for the given uri.
+  absl::Status StorePlaintextMessage(absl::string_view uri,
+                                     absl::string_view message);
+
+  // Returns a list of uris from received ReadRequest args.
+  std::vector<std::string> GetReadRequestUris();
+
   // Returns a list of received WriteRequest args.
   std::vector<std::vector<fcp::confidentialcompute::outgoing::WriteRequest>>
   GetWriteCallArgs();
 
  private:
+  // Map that stores the ReadResponse to return for a ReadRequest with a
+  // particular uri.
+  std::map<std::string, fcp::confidentialcompute::outgoing::ReadResponse>
+      uri_to_read_response_;
+
+  // List of uris from received ReadRequest args.
+  std::vector<std::string> read_request_uris_;
+
   // List of received WriteRequest args.
   std::vector<std::vector<fcp::confidentialcompute::outgoing::WriteRequest>>
       write_call_args_;
