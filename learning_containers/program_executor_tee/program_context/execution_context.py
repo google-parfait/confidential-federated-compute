392901d9d116751f588d56da2a0711d21b9e4757
Michael Reneer <michaelreneer@google.com>
2025-09-03 11:09:52 -0700

    Update dependencies for the `execution_context` target.
    * Remove unnecessary dependencies.
    * Add direct dependencies.
    * Update the requirements.in to include the direct dependencies.
    
    Change-Id: I5fbbf686d5a7338cfff0e2b4e31642e53398eb81

--- learning_containers/program_executor_tee/program_context/execution_context.py
+++ learning_containers/program_executor_tee/program_context/execution_context.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import base64
 from collections.abc import Callable
 import functools
 import os
