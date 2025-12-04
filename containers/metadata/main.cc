bdab993e7bcd4a8bc047c5e12b5fc27085f07360
Zoe Gong <zgong@google.com>
2025-12-04 15:25:23 -0800

    Add main binary and OCI bundle for Metadata Confidential Transform Server.
    This change introduces a `main.cc` file to run the Metadata Confidential
    Transform Server. It also adds the necessary BUILD rules to create an
    OCI image and runtime bundle for the metadata container, and includes
    this bundle in the _RELEASE_BINARIES so it will be imported.
    
    BUG:460521671
    Change-Id: Ie5ae2159db14ff9933008e8b4f12f4f517ea4e89

---
+++ containers/metadata/main.cc
@@ -0,0 +1,69 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include <string>
+
+#include "absl/log/check.h"
+#include "absl/log/log.h"
+#include "cc/containers/sdk/encryption_key_handle.h"
+#include "cc/containers/sdk/orchestrator_client.h"
+#include "cc/containers/sdk/signing_key_handle.h"
+#include "containers/fns/confidential_transform_server.h"
+#include "containers/metadata/metadata_map_fn.h"
+#include "grpcpp/security/credentials.h"
+#include "grpcpp/server.h"
+#include "grpcpp/server_builder.h"
+
+namespace confidential_federated_compute::metadata {
+
+namespace {
+
+using ::grpc::Server;
+using ::grpc::ServerBuilder;
+using ::oak::containers::sdk::InstanceEncryptionKeyHandle;
+using ::oak::containers::sdk::InstanceSigningKeyHandle;
+using ::oak::containers::sdk::OrchestratorClient;
+
+// Increase gRPC message size limit to 2GB.
+static constexpr int kChannelMaxMessageSize = 2 * 1000 * 1000 * 1000;
+
+void RunServer() {
+  std::string server_address("[::]:8080");
+
+  fns::FnConfidentialTransform service(
+      std::make_unique<InstanceSigningKeyHandle>(), ProvideMetadataMapFnFactory,
+      std::make_unique<InstanceEncryptionKeyHandle>());
+  ServerBuilder builder;
+  builder.SetMaxReceiveMessageSize(kChannelMaxMessageSize);
+  builder.SetMaxSendMessageSize(kChannelMaxMessageSize);
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server = builder.BuildAndStart();
+  LOG(INFO) << "Metadata Confidential Transform Server listening on "
+            << server_address << "\n";
+
+  CHECK_OK(OrchestratorClient().NotifyAppReady());
+  server->Wait();
+}
+
+}  // namespace
+
+}  // namespace
+   // confidential_federated_compute::metadata
+
+int main(int argc, char** argv) {
+  confidential_federated_compute::metadata::RunServer();
+  return 0;
+}
