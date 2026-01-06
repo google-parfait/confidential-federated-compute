068c88b1536c74674f0b468449a6026f29abacdf
Zachary Garrett <zachgarrett@google.com>
2026-01-06 10:28:28 -0800

    Add ahead-of-time graph optimization, disabling runtime graph optimization, and pre-execution graph hashing to the TrustedContext compiler pipeline.
    This also updates:
    - the commit hash of the tensorflow federated dependency.
    - the python tool chain from 3.10 to 3.11 to avoid pypi dependency
      issues where ml-dtypes dropped support for python 3.10.
    
    Change-Id: Idfe76555859d6e95b61aa10680b353f368de2ae1

--- program_tensorflow_containers/compilers.py
+++ program_tensorflow_containers/compilers.py
@@ -14,6 +14,18 @@
 
 import federated_language
 import tensorflow_federated as tff
+import tensorflow as tf
+
+
+# Turn on ahead-of-time graph optimization.
+grappler_config_pb = tf.compat.v1.ConfigProto()
+aggressive = grappler_config_pb.graph_options.rewrite_options.AGGRESSIVE
+rewrite_options = grappler_config_pb.graph_options.rewrite_options
+rewrite_options.memory_optimization = aggressive
+rewrite_options.constant_folding = aggressive
+rewrite_options.arithmetic_optimization = aggressive
+rewrite_options.loop_optimization = aggressive
+rewrite_options.function_optimization = aggressive
 
 
 def compile_tf_to_call_dominant(
@@ -23,7 +35,17 @@ def compile_tf_to_call_dominant(
   comp_bb, _ = tff.tensorflow.replace_intrinsics_with_bodies(
       comp.to_building_block()
   )
-  comp_bb = tff.framework.to_call_dominant(comp_bb)
+  cdf_comp_bb = tff.framework.to_call_dominant(comp_bb)
+  optimized_comp_bb, _ = tff.tensorflow.optimize_tensorflow_graphs(
+    cdf_comp_bb, grappler_config_pb
+  )
+  # Currently this compiler step will cause errors in some federated programs
+  # during ReduceDataset operations that complain about missing placeholders. So
+  # we disable for now.
+  # disabled_grappler_bb, _ = tff.tensorflow.transform_tf_call_ops_to_disable_grappler(
+  #   optimized_comp_bb
+  # )
+  final_comp_bb, _ =  tff.tensorflow.transform_tf_add_ids(optimized_comp_bb)
   return federated_language.framework.ConcreteComputation.from_building_block(
-      comp_bb
+      final_comp_bb
   )
